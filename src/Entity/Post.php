05ffdfa3139df1cf101a51e08e8dc7b6f81035d0
Mickael <mickaeldesclaux@gmail.com>
2023-06-07 10:38:38 +0200

    Starting from Studi live number 6

---
+++ src/Entity/Post.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity()]
+#[ORM\Table(name: "publication")]
+class Post 
+{
+
+    #[ORM\Id()]
+    #[ORM\GeneratedValue(strategy: "AUTO")]
+    #[ORM\Column(type: "integer")]
+    private int $id;
+
+    #[ORM\Column(type: "string", nullable: true, length: 180)]
+    private ?string $title = NULL;
+
+    #[ORM\Column(type: "text",length: 320)]
+    private string $content;
+
+    #[ORM\Column(type: "text")]
+    private ?string $image = NULL;
+
+    //#[ORM\ManyToOne(targetEntity: "App\Entity\User", inversedBy: "posts")]
+    //private $user;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /*public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }*/
+}
---
+++ src/Form/PostType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Url;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add("title", TextType::class, 
+            ["label" => "Titre", 
+            "required" => false,
+            "constraints" => [new Length (["min" => 0, "max" => 150, "minMessage" => "Le titre ne doit pas faire plus de 150 caractères", "maxMessage" => "Le titre ne doit pas faire plus de 150 caractères"
+            ])]])
+
+            ->add("content", TextareaType::class, 
+            ["label" => "Content", 
+            "required" => true,
+            "constraints" => [
+                new Length(["min" => 5, "max" => 320, "minMessage" => "Le contenu doit faire entre 5 et 320 caractères", "maxMessage" => "Le contenu doit faire entre 5 et 320 caractères"]),
+                new NotBlank(["message" => "Le contenu ne doit pas être vide !"
+                ])]])
+
+            ->add("image", UrlType::class, [
+                "label" => "URL de l'image", 
+                "required" => false,
+                "constraints" => [new Url(["message" => "L'image doit être une URL valide"])]
+            ]);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            "data_class" => Post::class
+        ]);
+    }
+}
